485311c462fa8fd936d28da98cdcf01296008999
Vladimir Kozinsky <kozinskytxc@gmail.com>
2022-11-12 20:10:58 +0400

    feat: add react router dom

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import s from "./App.module.scss";
+import { Routes, Route, } from "react-router-dom";
 import Header from "./components/Header/Header";
+import AddLiquidity from "./components/AddLiquidity/AddLiquidity";
+import Swap from "./components/Swap/Swap";
 
 function App() {
   return <div className={s.app}>
-    <Header />
+    <Routes>
+      <Route path='/' element={<AddLiquidity />} />
+      <Route path='/swap' element={<Swap />} />
+    </Routes>
   </div>;
 }
 
